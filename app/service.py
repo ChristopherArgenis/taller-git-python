8382b0a0e5352abd2c86179328f643e9aba45172
Christopher Argenis Preciado Silva <christopher.preciado.silva@uabc.edu.mx>
2025-03-11 11:42:40 -0700

    Actualizacion service.py
    Cambio de Nombre "Mario" a "Juan".

--- app/service.py
+++ app/service.py
@@ -2,5 +2,5 @@ from model import cliente
 
 clientes = cliente()
 
-customer1 = clientes("Mario", "out@gmail.com", "6461790054")
+customer1 = clientes("Juan", "out@gmail.com", "6461790054")
 customer1.message()
